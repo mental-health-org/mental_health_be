a6747b54d8504ec85961933ab00a017dd6404d29
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-18 13:30:59 -0700

    Add db to circleci

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mental_health',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
