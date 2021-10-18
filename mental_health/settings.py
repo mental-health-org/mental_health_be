6d5c13be1acd596764f45cd7ba16d42695393c66
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-17 20:15:23 -0700

    Add postgre

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'mental_health.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mental_health',
     }
 }
 
