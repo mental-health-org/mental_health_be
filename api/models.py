6560f62570e36c596603ff34ec5449ef471932cb
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-18 11:00:15 -0700

    Add users table

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length = 30, unique=True, error_messages={
+        'unique': 'Name is already taken'
+    })
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
