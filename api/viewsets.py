db0762be1293d5a24fdac651269904d073e99e7c
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-19 09:07:59 -0700

    Add tag viewset with list and retrieve

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,6 +1,6 @@
 from .models import User, Post, Tag
 from django.shortcuts import get_object_or_404
-from .serializers import QuestionsSerializer
+from .serializers import QuestionsSerializer, TagsSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 
@@ -15,4 +15,17 @@ class QuestionsViewSet(viewsets.ViewSet):
         queryset = Post.objects.all()
         question = get_object_or_404(queryset, pk=pk)
         serializer = QuestionsSerializer(question)
+        return Response(serializer.data)
+
+class TagsViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = Tag.objects.all()
+        serializer = TagsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Tag.objects.all()
+        tag = get_object_or_404(queryset, pk=pk)
+        serializer = TagsSerializer(tag)
         return Response(serializer.data)
