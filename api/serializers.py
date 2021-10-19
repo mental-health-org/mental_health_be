ae29355bb47d3b8b9cf0bf4800f03452780e93db
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-19 09:08:17 -0700

    Add tags serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,14 @@ from .models import User, Post, Tag
 
 
 class QuestionsSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Post
         fields = ('id', 'title', 'body', 'upvote', 'downvote', 'tagging', 'created_at')
+
+class TagsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
