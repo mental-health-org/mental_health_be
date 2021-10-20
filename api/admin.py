f4f70b872fa336ae1764e68225e3346c087663ec
Antonio King <antonio.j.king@gmail.com>
2021-10-20 08:57:01 -0600

    Create admin superuser for antonioking

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Post, Tag
+from .models import User, Post, Tag, Response
 
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Tag)
+admin.site.register(Response)
 
 # Register your models here.
