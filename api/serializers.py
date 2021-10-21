c499317e5a0f2494195dc09838e8d1638da3f863
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-20 23:57:14 -0700

    Add post serializer

--- api/serializers.py
+++ api/serializers.py
@@ -7,10 +7,17 @@ class QuestionsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'title', 'body', 'upvote', 'downvote', 'tagging', 'created_at')
+        fields = ('id', 'title', 'body', 'upvote', 'downvote', 'tagging', 'created_at', 'updated_at')
 
 class TagsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
         fields = ('id', 'name')
+
+class PostSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'user', 'title', 'body', 'upvote', 'downvote', 'created_at', 'updated_at')
