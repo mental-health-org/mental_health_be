8ebad4f53fd3185ed8f8fd028533e9b54bcd45db
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-20 23:57:36 -0700

    Add post http method to questions

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,7 +1,8 @@
 from .models import User, Post, Tag
 from django.shortcuts import get_object_or_404
-from .serializers import QuestionsSerializer, TagsSerializer
-from rest_framework import viewsets
+from django.http import HttpResponse
+from .serializers import QuestionsSerializer, TagsSerializer, PostSerializer
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 class QuestionsViewSet(viewsets.ViewSet):
@@ -16,6 +17,27 @@ class QuestionsViewSet(viewsets.ViewSet):
         question = get_object_or_404(queryset, pk=pk)
         serializer = QuestionsSerializer(question)
         return Response(serializer.data)
+
+    def create(self, request):
+        tags_data = request.data.copy().pop('tags')
+        post_data = request.data.copy()
+        del post_data['tags']
+
+        post_serializer = PostSerializer(data=post_data)
+        post_serializer.is_valid(raise_exception=True)
+        post_serializer.save()
+        new_post = Post.objects.filter(title=post_data['title']).last()
+
+        for tag in tags_data:
+            if not Tag.objects.filter(name=tag):
+                new_tag = Tag.objects.create(name=tag)
+                new_post.tagging.add(new_tag.id)
+            else:
+                existing_tag = Tag.objects.filter(name=tag).last()
+                new_post.tagging.add(existing_tag.id)
+
+        return Response(post_serializer.data, status=status.HTTP_201_CREATED)
+
 
 class TagsViewSet(viewsets.ViewSet):
 
