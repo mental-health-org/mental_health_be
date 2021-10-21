c31875b70584055957aaf5f87727723cf7327202
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-21 13:28:36 -0700

    Update static settings

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
