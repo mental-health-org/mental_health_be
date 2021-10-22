b3213709a2addc053f29d4da0c1e46ef8f6cadf8
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-21 18:47:44 -0700

    Add new secret key

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import os
 import django_heroku
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o#$td$t@=30u-o1vwyk6a)j(^#m3mc-*ol@63$6jr0pznld+kz'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
