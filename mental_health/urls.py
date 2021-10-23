322ffe5e314f5e14bcadf5726d72294ec5e6d31d
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-23 05:02:01 -0700

    Add question tag search url

--- mental_health/urls.py
+++ mental_health/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from .router import router
-
+from api.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('api/v1/filter/questions/', question_tags_search, name = 'filter_questions'),
 ]
