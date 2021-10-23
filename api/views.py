782cc35e15ae3598aa359c09b432e8a665ad3845
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-23 05:10:24 -0700

    Add question search view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import User, Post, Tag
+from .serializers import *
+from .models import *
+from rest_framework import filters, generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 import json
 
-# Create your views here.
+
+@api_view(('GET',))
+def question_search(request):
+
+    if request.GET.get('search') == None:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    search = request.GET.get('search')
+    title_results = Post.objects.filter(title__icontains=search)
+    serializer = QuestionsSerializer(title_results, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
