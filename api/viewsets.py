2e46e9caff3f02bd76ee4a56e48a4ab765ec106f
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-24 04:39:43 -0700

    Add destroy viewset to responses

--- api/viewsets.py
+++ api/viewsets.py
@@ -136,3 +136,10 @@ class ResponsesViewSet(viewsets.ViewSet):
         serializer.save()
         return FinalResponse(serializer.data, status=status.HTTP_200_OK)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = Response.objects.filter(id=self.kwargs['pk'])
+        self.perform_destroy(instance)
+        return FinalResponse(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
