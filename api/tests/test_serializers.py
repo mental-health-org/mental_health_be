ef68ae2b09bc7531eaed3a27a3f13a0df317aaef
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-24 04:29:21 -0700

    Add tag serializer fields test

--- api/tests/test_serializers.py
+++ api/tests/test_serializers.py
@@ -9,3 +9,9 @@ class TestTagsSerializer(TestCase):
 
         self.tag = Tag.objects.create(**self.tag_attributes)
         self.serializer = TagsSerializer(instance=self.tag)
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+
+        self.assertEqual(set(data.keys()), set(["id", "name"]))
+
