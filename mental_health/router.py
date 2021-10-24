03d3461f1e1d57474ab3933f7268a3a39b7d1b29
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-24 04:37:27 -0700

    Add responses view router

--- mental_health/router.py
+++ mental_health/router.py
@@ -1,4 +1,4 @@
-from api.viewsets import QuestionsViewSet, TagsViewSet, PostsViewSet, UsersViewSet
+from api.viewsets import *
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -6,3 +6,4 @@ router.register(r'questions', QuestionsViewSet, basename='questions')
 router.register(r'tags', TagsViewSet, basename='tags')
 router.register(r'posts', PostsViewSet, basename='posts')
 router.register(r'users', UsersViewSet, basename='users')
+router.register(r'responses', ResponsesViewSet, basename='responses')
