bbeeb511d1c299dc34a3c8c4d66cdc62fbdb896a
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-26 15:33:58 -0700

    Add joins tables to admin

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User, Post, Tag, Response
+from .models import *
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Response)
+admin.site.register(QuestionVotes)
+admin.site.register(ResponseVote)
