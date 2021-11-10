09b6fc38872d210299aa66cbdd28bf905d95f28c
Jason Knoll <jason.p.knoll@gmail.com>
2021-11-10 06:37:32 -0700

    Add account app to coverage

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import *
+from account.models import *
 
-# Register your models here.
+# From Accounts
 admin.site.register(User)
+
+# From API
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Response)
