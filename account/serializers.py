1f5b3a955085a02aa5aa30f2d3bd3f427f4cbecb
Jason Knoll <jason.p.knoll@gmail.com>
2021-11-10 06:48:50 -0700

    Add registration serializer

--- account/serializers.py
+++ account/serializers.py
@@ -7,3 +7,27 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("__all__")
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'title', 'email', 'password', 'password2']
+        extra_kwargs = {
+                    'password': {'write_only': True}
+        }
+
+    # Save override to ensure both passwords match
+    def save(self):
+        user = User(
+                username = self.validated_data['username'],
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        user.set_password(password)
+        user.save()
+        return user
