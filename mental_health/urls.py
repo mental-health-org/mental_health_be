fa26d1272121ddd9b80581d56aa4a219e8993076
Jason Knoll <jason.p.knoll@gmail.com>
2021-11-10 06:40:16 -0700

    Refactor url paths

--- mental_health/urls.py
+++ mental_health/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
+from django.contrib import admin
 from .router import router
 from api.views import *
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+    #Oauth
+    path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
+    # API Route Version Control
     path('api/v1/', include(router.urls)),
+    path('api/v1/', include('api.urls')),
+    # Custom Routes
     path('api/v1/filter/questions/', question_tags_search, name = 'filter_questions'),
     path('api/v1/search/questions/', question_search, name = 'search_questions'),
-    path('api/v1/', include('api.urls')),
+    # Authorization Route
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('/api/v1/account/', include('account.urls')),
+    # Password Management
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
