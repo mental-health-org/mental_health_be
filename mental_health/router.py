06ea546444345deb724e013bb6a06a8777e22e4c
Jason Knoll <jason.p.knoll@gmail.com>
2021-11-13 00:44:15 -0700

    Add questionflag and responseflag routes

--- mental_health/router.py
+++ mental_health/router.py
@@ -5,9 +5,11 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r'questions', QuestionsViewSet, basename='questions')
 router.register(r'qvote', QuestionVoteViewSet, basename='qvote')
+router.register(r'qflag', QuestionFlagViewSet, basename='qflag')
 
 router.register(r'responses', ResponsesViewSet, basename='responses')
 router.register(r'rvote', ResponseVoteViewSet, basename='rvote')
+router.register(r'rflag', ResponseFlagViewSet, basename='rflag')
 
 router.register(r'tags', TagsViewSet, basename='tags')
 
