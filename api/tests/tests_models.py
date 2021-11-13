ebc560fd77666f1525fb19bd0fae88c907dd89af
Jason Knoll <jason.p.knoll@gmail.com>
2021-11-13 00:36:25 -0700

    Add question flags model test

--- api/tests/tests_models.py
+++ api/tests/tests_models.py
@@ -94,3 +94,23 @@ class TestResponseVoteCase(TestCase):
 
     def test_questions_vote(self):
         self.assertEqual(2, self.response.votes.count())
+
+class TestQuestionsFlagCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username = 'Orson_Wells',
+                                        email = 'test@email.com',
+                                        title = 'professional',
+                                        password = '1a2b3c4d5e',
+                                        )
+        self.post = Post.objects.create( user = self.user, title = 'Test Title', body = 'ipsum lorem')
+
+        self.questionflag = QuestionFlag.objects.create(user = self.user, post = self.post, comment = "This is not a relevent question")
+
+    def test_questions_flag(self):
+        self.assertEqual(1, QuestionFlag.objects.count())
+        self.assertEqual(self.questionflag.user, self.user)
+        self.assertEqual(self.questionflag.post, self.post)
+        self.assertEqual(self.questionflag.status, 0)
+        self.assertEqual(self.questionflag.comment, "This is not a relevent question")
+
