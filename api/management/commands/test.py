89bc982e3a73062cb804343637d1561f99addcde
Jason Knoll <jason.p.knoll@gmail.com>
2021-11-13 07:45:55 -0700

    Remove api app

--- api/management/commands/test.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.commands.test import Command as BaseCommand
-
-
-class Command(BaseCommand):
-    def handle(self, *test_labels, **options):
-        # Wrap Django's built-in test command to always delete the database if
-        # it exists
-        options["interactive"] = False
-        return super().handle(*test_labels, **options)
